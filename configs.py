d36ecbd9e087e3bc7ca1c8700555ba95bdc4496b
Daniel Kappe <daniel.kappe@fh-bielefeld.de>
2021-04-17 11:56:08 +0200

    Add a couple of examples for testing

---
+++ configs.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 10:25:58 2021
+Provides a few Objects to play with and a function simplifing placing an object
+onto a simulation field.
+
+@author: dkappe
+"""
+import numpy as np
+
+
+def insert_pattern(base, pattern, offset=None):
+    """
+    Takes a base simulation field and places a given pattern with an offset
+    onto it. When offset is None, the object is placed into the middle
+
+    Parameters
+    ----------
+    base : numpy.ndarray
+        The base simulation field. Can already hold objects.
+    pattern : numpy.ndarray
+        The pattern to be placed. Should fit onto the simulation field
+    offset : (offset_x, offset_y), optional
+        offset in the x and y directions, from the middle. The default is None.
+
+    Raises
+    ------
+    ValueError
+        Error is raised, if the pattern does not fit onto the simulation field.
+
+    Returns
+    -------
+    field : TYPE
+        The simulation field with the pattern placed.
+    """
+    pass
+
+
+BLINKER = [[1, 1, 1]]
+
+BEACON = [[1, 1, 0, 0],
+          [1, 1, 0, 0],
+          [0, 0, 1, 1],
+          [0, 0, 1, 1]]
+
+PULSER = [[1, 1, 1, 1, 1, 1, 1, 1],
+          [1, 0, 1, 1, 1, 1, 0, 1],
+          [1, 1, 1, 1, 1, 1, 1, 1]]
+
+GOSPER = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
